3038e995987e35dd7f0335c22aba266bd53e964e
amirhkarami <amirhkarami@aut.ac.ir>
2026-07-23 18:17:41 +0330

    fixed a bug where you were not able to delete cities

--- src/main/java/org/example/secondhandbackend/controller/CityController.java
+++ src/main/java/org/example/secondhandbackend/controller/CityController.java
@@ -3,6 +3,7 @@ package org.example.secondhandbackend.controller;
 
 import org.example.secondhandbackend.model.City;
 import org.example.secondhandbackend.service.CityService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,5 +28,11 @@ public class CityController {
     public ResponseEntity<City> createCity(@RequestBody Map<String, String> body) {
         City city = cityService.create(body.get("name"));
         return ResponseEntity.ok(city);
+    }
+
+    @DeleteMapping("/cities/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable int id) {
+        cityService.delete(id);
+        return new ResponseEntity<>("city deleted", HttpStatus.OK);
     }
 }
